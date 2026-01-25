9366a4f2dbaf363d940dd8e13949269268a158d2
Simirall <37952374+Simirall@users.noreply.github.com>
2026-01-26 00:31:34 +0900

    トップページデザイン修正

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,6 +1,6 @@
 import { BroadcastIcon, ListDashesIcon } from "@phosphor-icons/react";
 import { createLazyFileRoute, Link } from "@tanstack/react-router";
-import { Button, HStack, Text } from "@yamada-ui/react";
+import { Button, Card, Text, VStack } from "@yamada-ui/react";
 
 export const Route = createLazyFileRoute("/")({
   component: RouteComponent,
@@ -8,27 +8,41 @@ export const Route = createLazyFileRoute("/")({
 
 function RouteComponent() {
   return (
-    <HStack>
-      <Button
-        as={Link}
-        colorScheme="sky"
-        endIcon={<ListDashesIcon size="28" weight="fill" />}
-        size="xl"
-        to="/list"
-        variant="subtle"
-      >
-        <Text>リスト一覧</Text>
-      </Button>
-      <Button
-        as={Link}
-        colorScheme="emerald"
-        endIcon={<BroadcastIcon size="28" weight="fill" />}
-        size="xl"
-        to="/antenna"
-        variant="subtle"
-      >
-        <Text>アンテナ一覧</Text>
-      </Button>
-    </HStack>
+    <VStack alignItems="center">
+      <Card.Root colorScheme="sky" maxW="2xl" size="xl" variant="subtle">
+        <Card.Header>リスト管理</Card.Header>
+        <Card.Body>
+          <p>リストの作成・編集・削除</p>
+        </Card.Body>
+        <Card.Footer alignSelf="end">
+          <Button
+            as={Link}
+            colorScheme="sky"
+            endIcon={<ListDashesIcon size="28" weight="fill" />}
+            size="xl"
+            to="/list"
+          >
+            <Text>リスト一覧</Text>
+          </Button>
+        </Card.Footer>
+      </Card.Root>
+      <Card.Root colorScheme="emerald" maxW="2xl" size="xl" variant="subtle">
+        <Card.Header>アンテナ管理</Card.Header>
+        <Card.Body>
+          <p>アンテナの作成・編集・削除</p>
+        </Card.Body>
+        <Card.Footer alignSelf="end">
+          <Button
+            as={Link}
+            colorScheme="emerald"
+            endIcon={<BroadcastIcon size="28" weight="fill" />}
+            size="xl"
+            to="/antenna"
+          >
+            <Text>アンテナ一覧</Text>
+          </Button>
+        </Card.Footer>
+      </Card.Root>
+    </VStack>
   );
 }
